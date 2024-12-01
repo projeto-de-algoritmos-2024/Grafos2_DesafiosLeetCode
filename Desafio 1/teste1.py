3240ea9d107ec86e6648463f51b0a953d3882e36
Julio Dourado <juliodourado419@gmail.com>
2024-12-01 19:19:01 -0300

    adcicionando funcoes heap na prim

--- Desafio 1/teste1.py
+++ Desafio 1/teste1.py
@@ -1,35 +1,67 @@
 class Solution(object):
+    # manipular a heap
+    def empurrar(self, heap, elemento):
+        heap.append(elemento)
+        self.subirElemento(heap, len(heap) - 1)
+
+    def retirar(self, heap):
+        if len(heap) == 1:
+            return heap.pop()
+        menor = heap[0]
+        heap[0] = heap.pop()
+        self.descerElemento(heap, 0)
+        return menor
+
+    def subirElemento(self, heap, indice):
+        pai = (indice - 1) // 2
+        while indice > 0 and heap[indice][0] < heap[pai][0]:
+            heap[indice], heap[pai] = heap[pai], heap[indice]
+            indice = pai
+            pai = (indice - 1) // 2
+
+    def descerElemento(self, heap, indice):
+        menor = indice
+        esquerda = 2 * indice + 1
+        direita = 2 * indice + 2
+
+        if esquerda < len(heap) and heap[esquerda][0] < heap[menor][0]:
+            menor = esquerda
+        if direita < len(heap) and heap[direita][0] < heap[menor][0]:
+            menor = direita
+
+        if menor != indice:
+            heap[indice], heap[menor] = heap[menor], heap[indice]
+            self.descerElemento(heap, menor)
+
     def minCostConnectPoints(self, pontos):
         quantidadePontos = len(pontos)
         custoTotal = 0
         pontosVisitados = [False] * quantidadePontos
-        custoMinimoParaConectar = [float('inf')] * quantidadePontos
-        custoMinimoParaConectar[0] = 0 
+        heap = []
 
-        for _ in range(quantidadePontos):
-            menorDistanciaAtual = float('inf')
-            indiceProximoPonto = -1
+        self.empurrar(heap, (0, 0))  # (custo, índice do ponto)
 
-            for i in range(quantidadePontos): # Selecionar o próximo ponto com menor custo
-                if not pontosVisitados[i] and custoMinimoParaConectar[i] < menorDistanciaAtual:
-                    menorDistanciaAtual = custoMinimoParaConectar[i]
-                    indiceProximoPonto = i
+        while heap:
+            custoAtual, pontoAtual = self.retirar(heap)
+            if pontosVisitados[pontoAtual]:
+                continue
 
-            pontosVisitados[indiceProximoPonto] = True
-            custoTotal += menorDistanciaAtual
+            pontosVisitados[pontoAtual] = True
+            custoTotal += custoAtual
 
-            for j in range(quantidadePontos): # Atualiza os custos para conectar os outros pontos
-                if not pontosVisitados[j]:
-                    distanciaX = abs(pontos[indiceProximoPonto][0] - pontos[j][0])
-                    distanciaY = abs(pontos[indiceProximoPonto][1] - pontos[j][1])
+            
+            for proximoPonto in range(quantidadePontos): # Atualiza a heap com os custos para os pontos não visitados
+                if not pontosVisitados[proximoPonto]:
+                    distanciaX = abs(pontos[pontoAtual][0] - pontos[proximoPonto][0])
+                    distanciaY = abs(pontos[pontoAtual][1] - pontos[proximoPonto][1])
                     distanciaTotal = distanciaX + distanciaY
-                    if distanciaTotal < custoMinimoParaConectar[j]:
-                        custoMinimoParaConectar[j] = distanciaTotal
+                    self.empurrar(heap, (distanciaTotal, proximoPonto))
+
         return custoTotal
 
-#teste
+# Caso de teste
 if __name__ == "__main__":
+    pontos = [[3, 12], [-2, 5], [-4, 1]]
     solution = Solution()
-    pontos = [[0, 0], [2, 2], [3, 10], [5, 2], [7, 0]]
     resultado = solution.minCostConnectPoints(pontos)
-    print("O Mínimo para conectar todos os pontos:", resultado)  # Nesse caso de teste deve ser 20
+    print("Custo mínimo para conectar todos os pontos:", resultado)  # 18
